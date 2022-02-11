21606ad09404bb239738bed7a8fe6011c2c3198f
Zhakee <kuttybek.zhandos@bk.ru>
2022-02-11 18:06:56 +0600

    lab

--- lab2/n.py
+++ lab2/n.py
@@ -1,19 +1,23 @@
-d=[]
-while(True):
-    a=input()
-    if a=='0':
-        break
-    else:
-        d.append(a)
-if len(d)%2==1:
-    for i in range(len(d)):
-        if i == len(d)/2-1:
-            break
-        else:
-            print(int(d[i])+int(d[len(d)-i-1]))
-else:
-    for i in range(len(d)):
-        if i == len(d)/2-1:
-    
-            print(d)
-       print(int(d[i])+int(d[len(d)-i-1]))
+d=[] 
+while(True): 
+    a=int(input()) 
+    if a==0: 
+        break 
+    else : 
+        d.append(a) 
+a = "" 
+if len(d)%2==1: 
+    for i in range(len(d)): 
+        if i == len(d)-i-1: 
+            a+=str(d[i]) 
+            break 
+        else: 
+            a+=str(d[i]+d[len(d)-i-1]) +" " 
+    print(a) 
+else: 
+    for i in range(len(d)): 
+        if i != len(d)-i-1: 
+            a+=str(d[i]+d[len(d)-i-1]) +" " 
+        if a.count(' ') == (len(d)/2): 
+            break 
+    print(a)
